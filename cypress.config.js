2f1333b3acf04a4d58e792ed2d10dfbc8c2ae2fc
Setup boilerplate <setup-boilerplate@example.com>
2024-08-22 13:50:45 +0000

    Initialize boilerplate fullstack-boilerplate-react-nextjs

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   env: {
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:3000/api',
   },
   e2e: {
     baseUrl: 'http://localhost:3000',
